ea219b58fd41b60561c269669e2b12669ed01cec
Hariharan Thiagarajan <hariharansmm@gmail.com>
2023-10-16 05:55:32 +0530

    Nineteenth Commit
    Updated Password Update Function
    Removed duplicate error message display in Password page

--- resources/views/admin/settings/admin-password.blade.php
+++ resources/views/admin/settings/admin-password.blade.php
@@ -39,38 +39,6 @@
             </div>
             <div class="row clearfix">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    @if(\Illuminate\Support\Facades\Session::has('error_message'))
-                        <div class="alert alert-danger alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <strong>Error: </strong> {{ \Illuminate\Support\Facades\Session::get('error_message') }}
-                        </div>
-                    @endif
-                    @if(\Illuminate\Support\Facades\Session::has('neutral_message'))
-                        <div class="alert alert-info alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <strong>Notice: </strong> {{ \Illuminate\Support\Facades\Session::get('neutral_message') }}
-                        </div>
-                    @endif
-                    @if(\Illuminate\Support\Facades\Session::has('success_message'))
-                        <div class="alert alert-success alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <strong>Success: </strong> {{ \Illuminate\Support\Facades\Session::get('success_message') }}
-                        </div>
-                    @endif
-                    @if($errors->any())
-                        <div class="alert alert-danger alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <strong>Error: </strong>
-                            <br>
-                            @foreach($errors->all() as $error)
-                                &emsp; &#x2022; {{ $error }}<br>
-                            @endforeach
-                        </div>
-                    @endif
-                </div>
-            </div>
-            <div class="row clearfix">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
                         <div class="header">
                             <h2>UPDATE PASSWORD</h2>
