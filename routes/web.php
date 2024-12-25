8eeae8e697ffe829c594be55a4b4401c013fb307
Hariharan Thiagarajan <hariharansmm@gmail.com>
2024-12-25 23:27:11 +1300

    Four Hundred and Sixty Second Commit
    Update Route Web

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function (): void {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -33,9 +33,9 @@ require __DIR__.'/auth.php';
 
 // Admin Route
 
-Route::prefix('/admin')->group(function() {
+Route::prefix('/admin')->group(function(): void {
     Route::match(['get','post'], 'login', [AdminController::class, 'login'])->name('Login');
-    Route::group(['middleware' => ['admin']], function() {
+    Route::group(['middleware' => ['admin']], function(): void {
         Route::get('dashboard', [AdminController::class, 'index'])->name('Dashboard');
         Route::match(['get','post'], 'password', [AdminController::class, 'password'])->name('Update Password');
         Route::match(['get','post'], 'account', [AdminController::class, 'account'])->name('Update Account');
